a6f28cb02ee896b1f7905bbf781b557a0247422a
kauanrossii <107654514+KauanMuriel@users.noreply.github.com>
2024-04-04 23:35:49 +0000

    Atividade 04-04-2024 finalizada

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+window.onload = async () => {
+    const queryString = new URLSearchParams(document.location.search);
+    const evolutionParameter = 'evolucao';
+
+    for (const [key, value] of queryString) {
+        if (key === evolutionParameter) {
+            
+            document.title += ` ${value}`;
+            document.querySelector('#pokemon-titulo').textContent += value;
+
+            fetch('https://pokeapi.co/api/v2/pokemon/wartortle')
+                .then(async (response) => {
+                    const pokemonInformation = await response.json();
+                    const pokemonElementSection = document.querySelector('#evolucao-section')
+                    const elementImage = document.createElement('img')
+                    elementImage.src = pokemonInformation.sprites.front_default;
+                    elementImage.alt = `Imagem da evolução ${value}`;
+                    elementImage.role = 'img';
+                    pokemonElementSection.appendChild(elementImage);
+                })
+        }
+    }
+}
